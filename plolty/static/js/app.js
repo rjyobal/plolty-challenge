b8a0c71b7ff0128c80f79b1e0789a139955139e4
rjyobal <rafaelyobal@msn.com>
2021-01-13 16:58:00 -0600

    Bubble plot v1

--- plolty/static/js/app.js
+++ plolty/static/js/app.js
@@ -6,7 +6,8 @@ function fillDropdown(){
     d3.json(jsonData).then((data) => {
         //console.log(data.names);
         let subjectNames = data.names;
-
+        //Add options to dropdown
+        d3.select("#selDataset").append("option").text('-Select Subject-')
         for(x in subjectNames){
             d3.select("#selDataset").append("option").text(subjectNames[x]);
         }
@@ -15,6 +16,7 @@ function fillDropdown(){
 
 //Get demographics based on selectedSubject
 function fillDemographics(selectedSubject){
+    d3.select('#sample-metadata').html('');
     d3.json(jsonData).then((data) => {
         for (x in data.metadata){
             if(data.metadata[x].id == selectedSubject){
@@ -35,19 +37,24 @@ function fillDemographics(selectedSubject){
 
 function optionChanged(selectedSubject){
     console.log(`Selected subject: ${selectedSubject}`);
-    fillDemographics(selectedSubject);
-    updatePlots(selectedSubject);
+    if(selectedSubject==='-Select Subject-'){
+        fillDemographics();
+        initPlots();
+    }else{
+        fillDemographics(selectedSubject);
+        updatePlots(selectedSubject);
+    }
 }
 
 function init(){
     fillDropdown();
-    fillDemographics(940);
+    fillDemographics();
     initPlots();
 }
 
 function initPlots(){
     console.log('Init Plots');
-    //Bar Plot
+    //Init Bar Plot
     var trace1 = {
         x:[1,2,3,4,5,6,7,8,9,10],
         y:['a','b','c','d','f','g','h','i','j','k'],
@@ -59,6 +66,22 @@ function initPlots(){
         title:'Top 10 OTUs'
     }
     Plotly.newPlot("bar", chartData, layout);
+    //Init Bubble Plot
+    var trace2 = {
+        x:[1,2,3,4,5,6,7,8,9,10],
+        y:[10,20,30,40,50,60,70,80],
+        text:['a','b','c','d','f','g','h','i','j','k'],
+        mode:'markers',
+        marker:{
+            size: [5,10,15,20,25,30,35,40,45,50],
+            color: [10,20,30,40,50,60,70,80]
+        }
+    }
+    var chartData2=[trace2];
+    var layout2={
+        title:'Bubble Chart'
+    }
+    Plotly.newPlot("bubble", chartData2, layout2);
 }
 
 function updatePlots(selectedSubject){
@@ -77,11 +100,23 @@ function updatePlots(selectedSubject){
                 top10ids = top10ids.map(i => 'OTU-' + i);
                 top10labels = top10labels.slice(0,10).reverse();
                 //console.log(top10labels);
-                //Restyle Plot
+
+                //Restyle Bar Plot
                 Plotly.restyle("bar","x",[top10]);
                 Plotly.restyle("bar","y",[top10ids]);
                 Plotly.restyle("bar","text",[top10labels]);
                 //console.log(data.samples[x].otu_ids);
+
+                //Restyle Bubble Plot
+                let x_bubble = data.samples[x].otu_ids
+                let y_bubble = data.samples[x].sample_values
+                let marker_bubble = {size:y_bubble, color:x_bubble}
+                let text_bubble = data.samples[x].otu_labels
+                //let marker_color = {color:x_bubble}
+                Plotly.restyle("bubble","x",[x_bubble]);
+                Plotly.restyle("bubble","y",[y_bubble]);
+                Plotly.restyle("bubble","marker",[marker_bubble]);
+                Plotly.restyle("bubble","text",[text_bubble]);
             }
         }
 
